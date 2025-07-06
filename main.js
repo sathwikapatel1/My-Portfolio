5b3b84ee6f21ce09df7563583fefbcae74522c28
Bhutham Sathwika <sathwikapatel25@gmail.com>
2025-07-06 09:45:02 +0530

    Initial commit of my portfolio project

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(" .text",{
-    strings: ["Full Stack Developer" , "Web Developer"],
+    strings: ["Full Stack Developer" , "Web Developer", "Data Analyst"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
